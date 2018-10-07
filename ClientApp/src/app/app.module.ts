1f660b33e9cb61df985cd6424887d22f3abdd177
Kleyton Ramos <kleytonmatosramos@gmail.com>
2018-10-07 12:08:51 -0300

    add template

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { TableComponent } from './table/table.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideBarComponent,
+    DashboardComponent,
+    LoginComponent,
+    TableComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'user', component: UserComponent},
+      { path: 'table', component: TableComponent},
+      { path: 'dashboard', component: DashboardComponent},
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
